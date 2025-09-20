8b6b006d526e2a35a7b58d46d0f90cd10c69bb22
Snoopie007 <steve00006@gmail.com>
2025-09-19 23:19:01 -0400

    update

--- src/components/navs/UserMenu.tsx
+++ src/components/navs/UserMenu.tsx
@@ -14,7 +14,6 @@ import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
 import { CreditCard, User } from "lucide-react";
 import { cn } from "@/libs/utils";
-import { useTheme } from "next-themes";
 
 
 const MenuItemStyle = 'flex flex-row items-center justify-start text-xs hover:bg-foreground/5'
@@ -22,7 +21,6 @@ const MenuItemStyle = 'flex flex-row items-center justify-start text-xs hover:bg
 export function UserMenu() {
     const session = useSession();
     const user = session.data?.user;
-    const { theme, setTheme } = useTheme();
 
     async function logOut() {
         await signOut();
@@ -59,19 +57,7 @@ export function UserMenu() {
                     </Link>
                 </DropdownMenuItem>
                 <DropdownMenuSeparator />
-                <DropdownMenuItem className="flex flex-col items-start justify-start space-y-2 focus:bg-transparent">
-                    <div className="text-xs  text-foreground/70  font-semibold">Theme</div>
-                    <ul className="space-y-0 justify-start w-full">
-                        {['light', 'dark', 'system'].map((t) => (
-                            <li key={t} className="text-xs font-medium capitalize flex items-center w-full p-1.5 hover:bg-foreground/5 rounded-sm gap-2" onClick={() => setTheme(t)}>
-                                <span className="w-3.5 flex justify-center">
-                                    {theme === t && <span className="size-1.5 bg-green-500 rounded-full"></span>}
-                                </span>
-                                <span className="text-xs">{t}</span>
-                            </li>
-                        ))}
-                    </ul>
-                </DropdownMenuItem>
+
                 <DropdownMenuSeparator />
                 <DropdownMenuItem onClick={logOut} className={cn(MenuItemStyle, 'font-semibold')}>
 
